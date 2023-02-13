3c79e4976013befa08b4c8b3b549bd7d627b4fec
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-02-13 20:21:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
 //your code here
-function findMinCost(input){
-    // split the input string into an array of ropelengths
-    let ropeLengths = input.split(',').map(function(length){
-        return parseInt(length, 10);
-    });
-    // sort the rope lengths in ascending order
-    ropeLengths.sort(function(a, b){
-        return a - b;
-    });
-    // initialize the minimum cost
-    let minCost = 0;
-
-    // keep contecting the two sorted ropes until there is only one rope left
-    while (ropeLengths.length > 1){
-        // take the two sorted ropes and sum their lengths
-        let newRope = ropeLengths[0] + ropeLengths[1];
-        minCost += newRope;
-
-        // remove the two sorted ropes from the array
-        ropeLengths.shift();
-        ropeLengths.shift();
-        
-        // insert the new rope into the sorted array of ropes
-        let index = 0;
-        while(index < ropeLengths.length && ropeLengths[index] < newRope){
-            index++;
-        }
-        ropeLengths.split(index, 0, newRope);
-    }
-    return minCost;
+function minCostToConnectRopes(arr){
+	let pq = new PriorityQueue({comparator: (a, b) => a - b});
+	for(let repo of arr){
+		pq.offer(rope);
+	}
+	let cost = 0;
+	while(pq.size()>1){
+		let rope1 = pq.poll();
+		let rope2 = pq.poll();
+		let combinedRepo = rope1 + rope2;
+		const += combinedRepo;
+		pq.offer(combinedRepo)
+	}
+	return cost;
 }
-
-let input = "4,2,7,6,9";
-let minCost = findMinCost(input);
-console.log(minCost);
