f2ec954a106fd96fcc105ea3624edb09b1b31a49
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-02-16 16:32:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function minCostToFormRopes(event){
 	event.preventDefault();
 	var inputElement = document.querySelector('input').value;
     var arr = inputElement.split(',');
-	arr.sort(fuction (a,b) {return a-b});
+	arr.sort(function (a,b) {return a-b});
 	
 	var cost = 0;
 	while(arr.length > 1){
@@ -13,7 +13,7 @@ function minCostToFormRopes(event){
 		cost += res;
 		arr.shift();
 		arr.shift();
-	    arr.sort(function (a,b) {result a-b});
+	    arr.sort(function (a,b) {return a-b});
 	}
 	document.getElementById("result").textContent = cost;
 }
